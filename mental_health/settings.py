87309dce46b9cde11761314ce33aa91a4e0c1971
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2025-01-05 20:38:34 +0000

    updatted template settings in settings.py

--- mental_health/settings.py
+++ mental_health/settings.py
@@ -69,7 +69,9 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
